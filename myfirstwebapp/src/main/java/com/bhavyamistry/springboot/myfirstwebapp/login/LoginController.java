69514008f1a7e2bc2f18cdc37a74694aedbee56f
bmistry2024 <bmistry@phoenixev.ai>
2025-11-18 16:44:32 -0500

    Query Params

---
+++ myfirstwebapp/src/main/java/com/bhavyamistry/springboot/myfirstwebapp/login/LoginController.java
@@ -0,0 +1,16 @@
+package com.bhavyamistry.springboot.myfirstwebapp.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+	@RequestMapping("login")
+	public String login(@RequestParam String name, ModelMap map) {
+		map.put("name", name);
+		return "login";
+	}
+}
---
+++ myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -0,0 +1,8 @@
+<html>
+	<head>
+		<title>Login Page - JSP</title>
+	</head>
+	<body>
+		Welcome to Login View ${name}
+	</body>
+</html>
