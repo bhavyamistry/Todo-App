2dabb8ed857132805166e386a5ca1402fbbfa179
bmistry2024 <bmistry@phoenixev.ai>
2025-11-25 16:25:27 -0500

    With H2 DB
     - Transformed List operations to H2 DB
     - Configured Spring Security to allow URLs

--- myfirstwebapp/src/main/java/com/bhavyamistry/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ myfirstwebapp/src/main/java/com/bhavyamistry/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -1,14 +1,16 @@
 package com.bhavyamistry.springboot.myfirstwebapp.security;
 
 import java.util.function.Function;
-
+import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -30,5 +32,19 @@ public class SpringSecurityConfiguration {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	// All URLs are protected
+	// A login form is shown for unauthorized requests
+	// CSRF should be disabled to allow H2 console
+	// H2 websites make use of Frames & Spring Security doesn't allow frames so
+	// might need to allow them as well
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+		http.formLogin(withDefaults());
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
+		return http.build();
 	}
 }
--- myfirstwebapp/src/main/java/com/bhavyamistry/springboot/myfirstwebapp/todo/Todo.java
+++ myfirstwebapp/src/main/java/com/bhavyamistry/springboot/myfirstwebapp/todo/Todo.java
@@ -2,12 +2,20 @@ package com.bhavyamistry.springboot.myfirstwebapp.todo;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
-
+//@Entity(name="TodoList")
+@Entity
 public class Todo {
 
+	@Id
+	@GeneratedValue
 	private int id;
+	
+//	@Column(name="name")
 	private String username;
 	
 	@Size(min=10, message="Enter atleast 10 characters")
@@ -16,6 +24,10 @@ public class Todo {
 	private LocalDate targetDate;
 	
 	private boolean done;
+	
+	public Todo() {
+		
+	}
 
 	public Todo(int id, String username, String description, LocalDate targetDate, boolean done) {
 		super();
