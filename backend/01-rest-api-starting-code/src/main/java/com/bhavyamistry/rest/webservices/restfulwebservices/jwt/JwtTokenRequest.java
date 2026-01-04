567fd0d4c6865d71b6c640e07e391616de73e484
BHAVYA MISTRY <bhavya.mstry@gmail.com>
2026-01-04 16:26:25 -0500

    file correction

--- backend/01-rest-api-starting-code/src/main/java/com/bhavyamistry/rest/webservices/restfulwebservices/jwt/JwtTokenRequest.java
+++
@@ -1,5 +0,0 @@
-package com.bhavyamistry.rest.webservices.restfulwebservices.jwt;
-
-public record JwtTokenRequest(String username, String password) {}
-
-
--- backend/01-rest-api-starting-code/src/main/java/com/bhavyamistry/rest/webservices/restfulwebservices/jwt/JwtTokenResponse.java
+++
@@ -1,5 +0,0 @@
-package com.bhavyamistry.rest.webservices.restfulwebservices.jwt;
-
-public record JwtTokenResponse(String token) {}
-
-
