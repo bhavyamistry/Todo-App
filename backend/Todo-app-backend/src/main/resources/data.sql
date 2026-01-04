1c78a4c870efcac2e8eeb9950df1786db6e7de44
BHAVYA MISTRY <58860047+bhavyamistry@users.noreply.github.com>
2026-01-04 17:41:27 -0500

    JPA Hibernate with H2 database (#13)
    * init commit
    
    Frontend
    Backend
    
    * todo-app
    
    * React Frontend
    
    * refactor renaming
    
    * Update .gitignore
    
    * .DS_Store files
    
    * JPA Hibernate with H2 database

---
+++ backend/Todo-app-backend/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO todo (username, description, target_date, done)
+VALUES ('bhavya', 'Learn AWS', CURRENT_DATE(), false);
+
+
+INSERT INTO todo (username, description, target_date, done)
+VALUES ('bhavya', 'Learn DevOps', CURRENT_DATE(), false);
+
+
+INSERT INTO todo (username, description, target_date, done)
+VALUES ('bhavya', 'Learn Full Stack Development', CURRENT_DATE(), false);
